b7779b624e06a332de988b2ede4f5c6fee386cdd
Dinas Suhadi <dinasputra@gmail.com>
2025-05-21 11:50:50 +0800

    Add Adamana icon & logo, and update sidebar header with Adamana branding

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,13 +1,14 @@
+import AppLogoFull from './app-logo-full';
 import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
     return (
         <>
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <AppLogoIcon className=" fill-current text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+                <span className="mb-0.5 truncate leading-none font-semibold">Adamana</span>
             </div>
         </>
     );
