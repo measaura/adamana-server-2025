b9030f6f5161eabeb2d1f14785bf8553216f3a49
Dinas Suhadi <dinasputra@gmail.com>
2025-05-23 20:22:58 +0800

    Add API route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TrackingController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
 Route::post('/updatedevice', [TrackingController::class, 'updateDevice']);
 Route::post('/promise', [TrackingController::class, 'handlePromise']);
